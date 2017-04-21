2ef9c0b4479d163dd3b32c34c24eadc162010bf0
Ophir LOJKINE <pere.jobs@gmail.com>
2017-04-21 19:45:59 +0300

    Add a TCP server to the agent

---
+++ superlogger/src/main/java/com/github/lovasoa/superlogger/TCPServer.java
@@ -0,0 +1,32 @@
+package com.github.lovasoa.superlogger;
+
+import java.io.*;
+import java.net.*;
+
+class TCPServer
+{
+  static int PORT = 6879;
+  public static void main(String argv[]) throws Exception
+  {
+    ServerSocket socket = new ServerSocket(PORT);
+    while (true) {
+      System.err.println("Waiting for connections on port " + PORT);
+      Socket conn = socket.accept();
+      System.err.println("New connection");
+      DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+
+      boolean isOpen = true;
+      String response;
+      while (isOpen) {
+        try {
+          out.writeBytes(new SystemInfo() + "\n");
+        } catch(SocketException e) {
+          System.err.println(e);
+          isOpen = false;
+        }
+        Thread.sleep(1000);
+      }
+      System.err.println("Connection closed");
+    }
+  }
+}
