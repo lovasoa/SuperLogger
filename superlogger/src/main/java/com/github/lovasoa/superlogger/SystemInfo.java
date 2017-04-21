ee663efca1b1d15f7e4a98308ec193e7b2a6649e
Ophir LOJKINE <pere.jobs@gmail.com>
2017-04-21 17:02:51 +0300

    Create a SystemInfo class

---
+++ superlogger/src/main/java/com/github/lovasoa/superlogger/SystemInfo.java
@@ -0,0 +1,41 @@
+package com.github.lovasoa.superlogger;
+
+import com.sun.management.OperatingSystemMXBean;
+import java.lang.management.ManagementFactory;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class SystemInfo 
+{
+    OperatingSystemMXBean osBean;
+
+    public SystemInfo() {
+        osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
+    }
+
+    public double cpu() {
+        return osBean.getSystemCpuLoad();
+    }
+
+    public double memory() {
+        return (double) osBean.getFreePhysicalMemorySize() /
+            osBean.getTotalPhysicalMemorySize();
+    }
+
+    public String computerName() {
+        try {
+            return InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException ex) {
+            return "unknown";
+        }
+    }
+
+    public String ip() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException ex) {
+            return "unknown";
+        }
+    }
+}
